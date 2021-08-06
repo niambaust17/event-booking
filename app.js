c6fba52302b29e4037c2e59ff9db38d7592932b9
unknown <niambaust17@gmail.com>
2021-08-07 00:44:55 +0600

    graphql setup

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) =>
-{
-    res.send('hello world')
-})
+
+app.use('/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+        type RootQuery{
+            events: [String!]!
+        }
+
+        type RootMutation{
+            createEvent(name: String): String
+        }
+
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+        rootValue: {
+            events: () =>
+            {
+                return ['Sleeping', 'Drawing', 'Running', 'Coding']
+            },
+            createEvent: (args) =>
+            {
+                const eventName = args.name;
+                return eventName
+            }
+        },
+        graphiql: true,
+    }))
 
 app.listen(3000)
